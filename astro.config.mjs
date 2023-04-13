bc27efaf4a79110f9ebc43a6d404f95e31a111ac
Liam Sanft <liam.sanft@gmail.com>
2023-04-13 18:10:59 +0200

    make site faster

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
+    compress(),
   ],
 });
